c862a982c5c400023b8f0aca8c414d2265ce2921
Alex Betts <alex.betts@gmail.com>
2011-11-09 12:07:55 -0600

    Forgot to add draw.h

---
+++ src/library/draw.h
@@ -0,0 +1,18 @@
+//-*-C++-*-
+// RSXGL - Graphics library for the PS3 GPU.
+//
+// Copyright (c) 2011 Alexander Betts (alex.betts@gmail.com)
+//
+// draw.h - Private constants, types and functions related to drawing to the framebuffer.
+
+#ifndef rsxgl_draw_H
+#define rsxgl_draw_H
+
+enum rsxgl_element_types {
+  RSXGL_ELEMENT_TYPE_UNSIGNED_INT = 0,
+  RSXGL_ELEMENT_TYPE_UNSIGNED_SHORT = 1,
+  RSXGL_ELEMENT_TYPE_UNSIGNED_BYTE = 2,
+  RSXGL_MAX_ELEMENT_TYPES = 3
+};
+
+#endif
