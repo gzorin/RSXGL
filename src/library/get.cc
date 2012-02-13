cacd20bd32ed29ea90f591f8be5c7c7d659b06f2
Alexander Betts <alex.betts@gmail.com>
2012-02-13 00:06:49 -0600

    Implemented glGetString functions.

--- src/library/get.cc
+++ src/library/get.cc
@@ -4,9 +4,10 @@
 //
 // get.cc - Implement glGet*() functions.
 
-#include "rsxgl_context.h"
-
 #include <GL3/gl3.h>
+
+#include "rsxgl_context.h"
+#include "error.h"
 
 #if defined(GLAPI)
 #undef GLAPI
@@ -36,4 +37,38 @@ glGetIntegerv (GLenum pname, GLint *params)
 GLAPI void APIENTRY
 glGetInteger64v (GLenum pname, GLint64 *params)
 {
+}
+
+GLAPI const GLubyte * APIENTRY
+glGetString (GLenum name)
+{
+  if(name == GL_VENDOR) {
+    RSXGL_NOERROR((const GLubyte *)"RSXGL");
+  }
+  else if(name == GL_RENDERER) {
+    RSXGL_NOERROR((const GLubyte *)"NV40");
+  }
+  else if(name == GL_VERSION) {
+    RSXGL_NOERROR((const GLubyte *)"3.1");
+  }
+  else if(name == GL_SHADING_LANGUAGE_VERSION) {
+    RSXGL_NOERROR((const GLubyte *)"1.30");
+  }
+  else if(name == GL_EXTENSIONS) {
+    RSXGL_NOERROR((const GLubyte *)"");
+  }
+  else {
+    RSXGL_ERROR(GL_INVALID_ENUM,0);
+  }
+}
+
+GLAPI const GLubyte * APIENTRY
+glGetStringi (GLenum name, GLuint index)
+{
+  if(name == GL_EXTENSIONS) {
+    RSXGL_ERROR(GL_INVALID_VALUE,0);
+  }
+  else {
+    RSXGL_ERROR(GL_INVALID_ENUM,0);
+  }
 }
