b2e08016af3509462041fa7a70b958b1c9b02156
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2026-04-28 13:15:39 +0000

    Fix GL_UNSIGNED_INT_8_8_8_8_REV

--- src/library/st_format.c
+++ src/library/st_format.c
@@ -1818,7 +1818,7 @@ rsxgl_choose_source_format(GLenum format,GLenum type)
     if(type == GL_UNSIGNED_INT_8_8_8_8) {
       return PIPE_FORMAT_A8B8G8R8_UNORM;
     }
-    if(type == GL_UNSIGNED_INT_8_8_8_8) {
+    if(type == GL_UNSIGNED_INT_8_8_8_8_REV) {
       return PIPE_FORMAT_R8G8B8A8_UNORM;
     }
     else if(type == GL_BYTE) {
